8008bab8beec0ce083f42e3b0586878e74589d11
thereisasunthing <kyi.djtp@gmail.com>
2026-07-28 16:05:29 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
      (v1 -> v2) 그래야 사용자 휴대폰의 캐시가 새 파일로 교체됩니다.
    ============================================ */
 
-const CACHE_NAME = "angdu-moim-v1";
+const CACHE_NAME = "angdu-moim-v2";
 
 const FILES_TO_CACHE = [
   "./",
